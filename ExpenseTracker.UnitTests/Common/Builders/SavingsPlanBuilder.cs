f7a89b8632b6b2b1ce4620c0683c23629855f479
Vukasin Macic <vukasin.macic@vega.local>
2025-12-17 14:14:57 +0100

    Basic tests for SavingsPlanCalculator and SavingsPlanService (scenarios running out of money, hitting target goal)

---
+++ ExpenseTracker.UnitTests/Common/Builders/SavingsPlanBuilder.cs
@@ -0,0 +1,25 @@
+﻿using ExpenseTracker.WebApi.Domain.Entities;
+
+namespace ExpenseTracker.UnitTests.Common.Builders;
+
+public class SavingsPlanBuilder
+{
+    private SavingsPlan _plan = new()
+    {
+        UserId = Guid.NewGuid(),
+        TargetAmount = 1000,
+        IsActive = true
+    };
+
+    public SavingsPlanBuilder WithAmount(decimal amount) {
+        _plan.TargetAmount = amount;
+        return this;
+    }
+
+    public SavingsPlanBuilder Deactivated() {
+        _plan.IsActive = false;
+        return this;
+    }
+
+    public SavingsPlan Build() => _plan;
+}
---
+++ ExpenseTracker.UnitTests/Savings/SavingsPlanCalculatorTests.cs
@@ -0,0 +1,28 @@
+﻿using ExpenseTracker.WebApi.Application.Services;
+using FluentAssertions;
+using Xunit;
+
+namespace ExpenseTracker.UnitTests.Savings;
+
+public class SavingsPlanCalculatorTests
+{
+    [Fact]
+    public void CalculateContributions_SplitsAmountAcrossMonths()
+    {
+        var calculator = new SavingsPlanCalculator();
+
+        var targetAmount = 1200m;
+        var start = new DateTime(2025, 1, 1);
+        var target = new DateTime(2025, 7, 1);
+        
+        var contributions = calculator.CalculateContributions(
+            targetAmount,
+            start,
+            target
+        ).ToList();
+        
+        contributions.Should().HaveCount(7);
+        contributions.Sum(c => c.PlannedAmount).Should().Be(targetAmount);
+    }
+}
+
